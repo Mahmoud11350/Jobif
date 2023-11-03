c28f0bb93bd81585156ef745adf8a944324c0f70
mahmoud11350 <mahmoudsalah11350@gmail.com>
2023-11-03 22:18:03 +0200

    Remove file

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import helmet from "helmet";
 import express from "express";
 import fileUpload from "express-fileupload";
 import { v2 as cloudinary } from "cloudinary";
-import demoUserMiddleware from "./middleware/demoUserMiddleware.js";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 cloudinary.config({
   cloud_name: "fullstack-mern-developer",
@@ -30,6 +32,8 @@ import cookieParser from "cookie-parser";
 if (process.env.NODE_ENV === "DEVELOPMENT") {
   app.use(morgan("dev"));
 }
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
@@ -37,13 +41,15 @@ app.use(
     useTempFiles: true,
   })
 );
-// app.use(cors({ credentials: true, origin: true }));
-// app.use(helmet());
 
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", [authMiddleware], userRoutes);
 app.use("/api/v1/jobs", [authMiddleware], jobsRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 // post middleware
 app.use(notFound);
